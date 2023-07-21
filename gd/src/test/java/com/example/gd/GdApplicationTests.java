766daf73139659575c49472f9e382bf30cbfc8af
MichaelB94 <124793101+MichaelB94@users.noreply.github.com>
2023-07-21 16:26:33 +0200

    Initial commit

---
+++ gd/src/test/java/com/example/gd/GdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
